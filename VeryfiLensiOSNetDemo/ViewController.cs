948fb4c26f856483888aec8b925edac704bdfbe6
Douglas Poveda <douglaspoveda@veryfi.com>
2023-08-15 17:00:20 -0500

    Add iOS demo

---
+++ VeryfiLensiOSNetDemo/ViewController.cs
@@ -0,0 +1,205 @@
+﻿using CoreFoundation;
+using VeryfiLensiOS;
+namespace iOSVeryfiTestApp;
+
+public partial class ViewController : UIViewController
+{
+    const string CLIENT_ID = "XXXX"; //Replace with your clientId
+    const string AUTH_USRNE = "XXXX"; //Replace with your username
+    const string AUTH_API_K = "XXXX"; //Replace with your apiKey
+    const string API_URL = "XXXX"; //Replace with your url
+
+    public ViewController() : base(nameof(ViewController), null)
+    {
+    }
+
+    public ViewController(IntPtr handle) : base(handle)
+    {
+    }
+
+    public override void ViewDidLoad()
+    {
+        base.ViewDidLoad();
+        this.ShowLogs("Configuring Lens");
+        SetUpVeryfiLens();
+        SetUpVeryfiLensDelegate();
+    }
+
+    partial void OpenLens(NSObject sender)
+    {
+        VeryfiLens.Shared.ShowCameraIn(this);
+    }
+
+    private void SetUpVeryfiLensDelegate()
+    {
+        VeryfiLens.Shared.Delegate = new VeryfiLensDelegateListener(this);
+    }
+
+    private void SetUpVeryfiLens()
+    {
+        var categories = new string[]
+        {
+                "Meals",
+                "Entertainment",
+                "Supplies"
+        };
+        var documentTypes = new string[]
+        {
+                "long_receipt",
+                "receipt",
+                "bill",
+                "other"
+        };
+        VeryfiLensSettings veryfiLensSettings = new VeryfiLensSettings
+        {
+            ShowDocumentTypes = true,
+            BlurDetectionIsOn = true,
+            AutoLightDetectionIsOn = false,
+            AutoCaptureIsOn = false,
+            BackupDocsToGallery = true,
+            AutoDocDetectionAndCropIsOn = true,
+            AutoCropGalleryIsOn = false,
+            EmailCCIsOn = false,
+            ConfidenceDetailsIsOn = true,
+            CloseCameraOnSubmit = true,
+            RotateDocIsOn = true,
+            ReturnStitchedPDF = true,
+            StitchIsOn = true,
+            MultipleDocumentsIsOn = true,
+            LocationServicesIsOn = true,
+            MoreMenuIsOn = true,
+            DataExtractionEngine = DataExtractionEngine.VeryfiCloudAPI,
+            ShieldProtectionIsOn = true,
+            Categories = categories,
+            IsProduction = false,
+            OriginalImageMaxSizeInMB = 2.5f,
+            StitchedPDFPixelDensityMultiplier = 2.0f,
+            SaveLogIsOn = true,
+            ShareLogsIsOn = true,
+            DocumentTypes = documentTypes,
+            AutoSubmitDocumentOnCapture = false,
+            AutoRotateIsOn = true,
+            ExternalId = "testExternalId1234",
+            GalleryIsOn = true,
+            AllowSubmitUndetectedDocsIsOn = true
+        };
+        var veryfiLensCredentials = new VeryfiLensCredentials(CLIENT_ID, AUTH_USRNE, AUTH_API_K, API_URL);
+        Action<bool> completion = (success) =>
+        {
+            if (success)
+            {
+                DispatchQueue.MainQueue.DispatchAsync(() =>
+                {
+                    this.ShowLogs("Lens configured");
+                });
+            }
+        };
+
+        VeryfiLens.Shared.ConfigureWith(veryfiLensCredentials, veryfiLensSettings, completion);
+        SetCategories();
+        SetCustomers();
+        SetTags();
+        SetCostCodes();
+
+    }
+
+    private void SetCostCodes()
+    {
+        var costCodes = new VeryfiLensCostCode[10];
+        for (int i = 0; i < 10; i++)
+        {
+            var job = new VeryfiLensCostCode
+            {
+                CostCodeId = i,
+                Code = "00" + i,
+                Name = "Cost " + i
+            };
+            costCodes[i] = job;
+        }
+        VeryfiLens.Shared.Settings.CostCodes = costCodes;
+    }
+
+    private void SetTags()
+    {
+        var tags = new VeryfiLensTag[10];
+        for (int i = 0; i < 10; i++)
+        {
+            var tag = new VeryfiLensTag
+            {
+                TagId = i,
+                Name = "Tag Number " + i
+            };
+            tags[i] = tag;
+        }
+        VeryfiLens.Shared.Settings.CoreTags = tags;
+    }
+
+    private void SetCustomers()
+    {
+        var customers = new VeryfiLensCPModel[10];
+        for (int i = 0; i < 10; i++)
+        {
+            var customer = new VeryfiLensCPModel
+            {
+                ProjectId = i,
+                CustomerId = i,
+                CustomerName = "Customer " + i
+            };
+            customers[i] = customer;
+        }
+        VeryfiLens.Shared.Settings.Customers = customers;
+    }
+
+    private void SetCategories()
+    {
+        var categories = new VeryfiLensCategory[10];
+        for (int i = 0; i < 10; i++)
+        {
+            VeryfiLensCategory category = new VeryfiLensCategory
+            {
+                CategoryId = i,
+                Name = "Category Number " + i,
+                Type = "mo_expense"
+            };
+            categories[i] = category;
+        }
+        VeryfiLens.Shared.Settings.CategoriesList = categories;
+    }
+
+    private void ShowLogs(string log)
+    {
+        var text = Logs.Text + log + "\n";
+        Logs.Text = text;
+    }
+
+    private class VeryfiLensDelegateListener : VeryfiLensDelegate
+    {
+        private ViewController viewController;
+
+        public VeryfiLensDelegateListener(ViewController viewController)
+        {
+            this.viewController = viewController;
+        }
+
+        public override void VeryfiLensClose(NSDictionary<NSString, NSObject> json)
+        {
+            viewController.ShowLogs(json.ToString());
+        }
+
+        public override void VeryfiLensError(NSDictionary<NSString, NSObject> json)
+        {
+            viewController.ShowLogs(json.ToString());
+        }
+
+        public override void VeryfiLensSuccess(NSDictionary<NSString, NSObject> json)
+        {
+            viewController.ShowLogs(json.ToString());
+        }
+
+        public override void VeryfiLensUpdate(NSDictionary<NSString, NSObject> json)
+        {
+            viewController.ShowLogs(json.ToString());
+        }
+    }
+}
+
---
+++ VeryfiLensiOSNetDemo/ViewController.designer.cs
@@ -0,0 +1,30 @@
+﻿// WARNING
+//
+// This file has been generated automatically by Visual Studio to store outlets and
+// actions made in the UI designer. If it is removed, they will be lost.
+// Manual changes to this file may not be handled correctly.
+//
+using Foundation;
+using System.CodeDom.Compiler;
+
+namespace iOSVeryfiTestApp
+{
+	[Register ("ViewController")]
+	partial class ViewController
+	{
+        [Outlet]
+        UIKit.UITextView Logs { get; set; }
+
+        [Action ("OpenLens:")]
+		partial void OpenLens (Foundation.NSObject sender);
+		
+		void ReleaseDesignerOutlets ()
+		{
+            if (Logs != null)
+            {
+                Logs.Dispose();
+                Logs = null;
+            }
+        }
+	}
+}
