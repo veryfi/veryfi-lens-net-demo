f081dceccf836ea3a9d6b6ef9d92fad73e8eb214
Alejandro Maldonado <alejandromaldonado@veryfi.com>
2026-09-24 18:06:16 -0500

    Update to 2.0.60

--- VeryfiLensiOSNetDemo/AppDelegate.cs
+++ VeryfiLensiOSNetDemo/AppDelegate.cs
@@ -1,15 +1,24 @@
 ﻿namespace VeryfiLensiOSNetDemo;
 
-[Register ("AppDelegate")]
-public class AppDelegate : UIApplicationDelegate {
-	public override UIWindow? Window {
-		get;
-		set;
-	}
+[Register("AppDelegate")]
+public class AppDelegate : UIApplicationDelegate
+{
+    public override bool FinishedLaunching(UIApplication application, NSDictionary launchOptions)
+    {
+        return true;
+    }
 
-	public override bool FinishedLaunching (UIApplication application, NSDictionary launchOptions)
-	{
-		return true;
-	}
+    public override UISceneConfiguration GetConfiguration(
+        UIApplication application,
+        UISceneSession connectingSceneSession,
+        UISceneConnectionOptions options)
+    {
+        return new UISceneConfiguration(
+            "Default Configuration",
+            connectingSceneSession.Role)
+        {
+            DelegateType = typeof(SceneDelegate),
+            Storyboard = UIStoryboard.FromName("Main", null)
+        };
+    }
 }
-
